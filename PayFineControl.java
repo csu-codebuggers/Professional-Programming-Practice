ccffff20d9f4459c190d9012d337d61c04deee85
Jikesh <munikargks@gmail.com>
2019-08-16 22:51:59 +1000

    Update PayFineControl.java
    changed some variable names

--- PayFineControl.java
+++ PayFineControl.java
@@ -46,18 +46,18 @@ public class PayFineControl {
 	}
 
 
-	public double PaY_FiNe(double AmOuNt) {
-		if (!StAtE.equals(CONTROL_STATE.PAYING)) {
+	public double PaY_FiNe(double amount) { //changes AmOuNt to amount
+		if (!state.equals(CONTROL_STATE.PAYING)) { //changed StAtE to state
 			throw new RuntimeException("PayFineControl: cannot call payFine except in PAYING state");
 		}	
-		double ChAnGe = MeMbEr.Pay_Fine(AmOuNt);
-		if (ChAnGe > 0) {
-			Ui.DiSplAY(String.format("Change: $%.2f", ChAnGe));
+		double change = MeMbEr.Pay_Fine(amount); //changed AmOuNt to amount and ChAnGe to change
+		if (change > 0) { //changed ChAnGe to change
+			ui.DiSplAY(String.format("Change: $%.2f", change)); //changed ChAnGe to change and Ui to ui
 		}
-		Ui.DiSplAY(MeMbEr.toString());
-		Ui.Set_State(PayFineUI.UI_STATE.COMPLETED);
-		StAtE = CONTROL_STATE.COMPLETED;
-		return ChAnGe;
+		ui.DiSplAY(member.toString()); //changed Ui to ui and MeMbEr to member
+		ui.Set_State(PayFineUI.UI_STATE.COMPLETED); //changed Ui to ui
+		state = CONTROL_STATE.COMPLETED;  //changed StAtE to state
+		return change;
 	}
 	
 
